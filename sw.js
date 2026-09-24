527ecfc43428caf3cb145fb678ca065dde875e11
syazie93 <syazie93@icloud.com>
2026-09-24 11:34:50 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+// Offline support: serve from cache, refresh in the background.
+const CACHE = 'logbook-v1';
+const CORE = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-180.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.open(CACHE).then(async cache => {
+    const cached = await cache.match(e.request, {ignoreSearch: true});
+    const network = fetch(e.request).then(res => {
+      if (res && (res.ok || res.type === 'opaque')) cache.put(e.request, res.clone());
+      return res;
+    }).catch(() => cached);
+    return cached || network;
+  }));
+});
